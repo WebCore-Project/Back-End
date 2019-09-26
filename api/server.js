08c5725e645444ba9bf2ced6af5a59702458c753
Bilguun Tse <bbg.1015@gmail.com>
2019-09-26 11:24:00 -0500

    deleted users router

--- api/server.js
+++ api/server.js
@@ -4,7 +4,6 @@ const helmet = require('helmet');
 
 const authRouter = require('../auth/auth-router.js');
 const addVacRouter = require('../vacations/vacation-router.js');
-const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
@@ -14,6 +13,5 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/vacations', addVacRouter);
-server.use('/api', usersRouter);
 
 module.exports = server;
